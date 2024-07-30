b6a5f63d20b343bb40e3fa7f2b15ca1a7b0c5083
hrrsshuu <148679895+hrrsshuu@users.noreply.github.com>
2024-07-30 18:54:41 +0530

    commit

---
+++ index.js
@@ -0,0 +1,68 @@
+let tasks = [];
+
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text: text, completed: false });
+        taskInput.value = "";
+        updateTaskList();
+    }
+};
+
+const updateTaskList = () => {
+    const taskList = document.querySelector(".task-list");
+    const progressBar = document.getElementById("progress");
+    const numbers = document.getElementById("numbers");
+
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+        listItem.className = "taskItem";
+        listItem.innerHTML = `
+            <div class="task ${task.completed ? "completed" : ""}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""} />
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="./img/bin.jpg" onClick="editTask(${index})" />
+                 <img src="./img/download.jpg" onClick="deleteTask(${index})"/>
+            </div>
+        `;
+
+        listItem.querySelector(".checkbox").addEventListener("change", () => toggleTaskComplete(index));
+        taskList.appendChild(listItem);
+    });
+
+    const completedTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = totalTasks === 0 ? 0 : (completedTasks / totalTasks) * 100;
+
+    progressBar.style.width = `${progress}%`;
+    numbers.innerText = `${completedTasks} / ${totalTasks}`;
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = tasks[index].txt;
+
+    tasks.splice(index, 1);
+    updateTaskList();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTaskList();
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskList();
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+    e.preventDefault();
+    addTask();
+});
